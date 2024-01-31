6c58e737b429210388caad80fb6b85406dc9e62f
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-01-31 20:02:44 +0000

    created base files

---
+++ func.py
@@ -0,0 +1,7 @@
+download_path = "/home/mikolaj/Downloads/"
+documents_path = "/home/mikolaj/Documents/"
+pictures_path = "/home/mikolaj/Pictures/"
+videos_path = "/home/mikolaj/Videos/"
+
+def getAllDownloads(download_path):
+    pass
